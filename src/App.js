38db15500ab87ba765ddfb661455681e7f1e7e99
= <=>
2021-10-06 14:59:29 +0200

    adding eslint and prettier // TODO : make eslint less strict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/header/Header.tsx'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => (
+  <header>
+    <nav className="d-flex ">
+      <div className="logo-container">ALTEACARTO</div>
+    </nav>
+  </header>
+)
+
+export default Header
